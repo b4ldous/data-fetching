0e039dd00623a306fb4d7112ce56bc0a819ad056
b4ldous <baldous@protonmail.com>
2022-06-18 23:46:52 -0500

    fetching data using hacker api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState("");
+  const [url, setUrl] = useState(
+    "https://hn.algolia.com/api/v1/search?query=redux"
+  );
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios(url);
+
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [url]);
+
   return (
     <>
-    template
-     
+      <form
+        onSubmit={(event) => {
+          setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`);
+
+          event.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+          placeholder="Search"
+        />
+        <button type="submit">Search</button>
+      </form>
+      {isError && <div>Something went wrong ...</div>}
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <ul>
+          {data.hits.map((item) => (
+            <li key={item.objectID}>
+              <a target="_blank" rel="noreferrer" href={item.url}>
+                {item.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
